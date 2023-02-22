b4f53c4fc1750914996b6ee9ae1e64efc63adcfe
Maciej Malysz <mmalysz@cisco.com>
2023-02-22 09:56:36 +0100

    tested on Terraform v1.3.9

--- modules/terraform-nso-mpls-vpn/versions.tf
+++ modules/terraform-nso-mpls-vpn/versions.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.1.0"
+  required_version = ">= 1.3.0"
 
   required_providers {
     nso = {
@@ -7,6 +7,4 @@ terraform {
       version = ">= 0.1.0"
     }
   }
-
-  experiments = [module_variable_optional_attrs]
 }
